3a3b136ba7ad039be19c6fd32a14328cbf867ad9
chandandeepJS <chandandeep@responseiq.com>
2022-02-26 22:42:48 +0530

    changes

--- src/model/sync.ts
+++ src/model/sync.ts
@@ -1,13 +1,13 @@
 import {
 
-   Coin,
-} from '.';
+   User,
+} from './user.model';
 import sequelize from './db';
 
 export async function syncAll(force = false): Promise<void> {
    try {
       await sequelize.sync({ force });
-      await Coin.findOne();
+      await User.findOne();
 
    } catch (err) {
       console.log('Error while syncing models', err);
